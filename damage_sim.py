fa24bbdfa8a205e51217260fd45bfa8d548c5712
Arrok <ZmanX102+ArrokGit@gmail.com>
2022-01-25 20:08:22 -0500

    Initial files commit. Contains main sim file and weapon class file

---
+++ damage_sim.py
@@ -0,0 +1,28 @@
+#A program to help simulating damage per round in the Lancer RPG
+import random
+
+def roll_accuracy(value):
+	maxroll = 1
+	for x in range(value):
+		roll = random.randint(1,6)
+		if roll == 6: return 6
+		if roll > maxroll: maxroll = roll
+	return maxroll
+		
+
+def calc_accuracy(num_accuracy = 0, num_difficulty = 0):
+	net = num_accuracy-num_difficulty
+	if net == 0: return 0
+	if net>0:
+		return roll_accuracy(net)
+	elif net<0:
+		return -1*roll_accuracy(-1*net)
+		
+def roll_d20():
+	return random.randint(1,20);
+	
+#def roll_damage(hits, crits , flat_bonus):
+	
+
+var_a = calc_accuracy()
+print(var_a)
---
+++ weapon_class.py
@@ -0,0 +1,2 @@
+#Provides weapon class definitoin for weapons
+#and functions for parsing lancer weapon .json objects
